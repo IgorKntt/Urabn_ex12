c831e09c56826add133c307c6fd6dab62cfc937e
IgorKntt <igor.gorskin@hotmail.com>
2024-12-13 13:43:09 +0500

    .js files converted to .ts

---
+++ src/types/enums.ts
@@ -0,0 +1,17 @@
+export enum NewsCategory {
+    Business = 'business',
+    Entertainment = 'entertainment',
+    General = 'general',
+    Health = 'health',
+    Science = 'science',
+    Sports = 'sports',
+    Technology = 'technology',
+}
+
+export enum ErrorType {
+    Auth = 'auth',
+    NotFound = 'not_found',
+    Server = 'server',
+    Unknown = 'unknown',
+
+}
